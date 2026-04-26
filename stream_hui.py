62776782b04ed72aa2de1f87c355bf21ceba3a61
fhzhkunming <46935214+fhzhkunming@users.noreply.github.com>
2026-04-25 23:57:30 -0400

    Add files via upload
    update .py script

--- stream_hui.py
+++ stream_hui.py
@@ -17,17 +17,26 @@ import pandas as pd
 import time
 import os
 
-# Create the stream input directory if it doesn't exist
-if not os.path.exists('stream_input'):
-    os.makedirs('stream_input')
+# 1. Ensure the input directory exists
+input_dir = '/content/stream_input'
+if not os.path.exists(input_dir):
+    os.makedirs(input_dir)
 
-df = pd.read_csv("/content/power_streaming_data.csv")
-# check if the data read in correctly
-df.head(10)
+# 2. Clear existing files in the stream_input directory
+for f in os.listdir(input_dir):
+    os.remove(os.path.join(input_dir, f))
 
-# randomly sample and write 20 csv files
-for i in range(20):
-    sample = df.sample(n = 5)
-    sample.to_csv(f"stream_input/sample_{i}.csv", index = False)
-    time.sleep(10) # pause 10 seconds between outputs
+# 3. Load the full streaming dataset
+df = pd.read_csv("https://www4.stat.ncsu.edu/~online/datasets/power_streaming_data.csv")
+
+# 4. wait for Spark to start watching
+time.sleep(10)
+
+# 5. randomly sample new files
+for i in range(10):
+    sample = df.sample(n = 5)                                             # sample 5 rows
+    sample.to_csv(f"/content/stream_input/sample_{i}.csv", index = False) # write the sample file to input folder
+    time.sleep(10)                                                        # pause 10 seconds between outputs
+
+print("Starting data generation...")
 
