dc1f8cc4a63a17334900f71288c9afce28a35441
fhzhkunming <46935214+fhzhkunming@users.noreply.github.com>
2026-04-30 10:42:59 -0400

    Add files via upload
    final updates for the notebook and python script

--- stream_hui.py
+++ stream_hui.py
@@ -1,5 +1,5 @@
 
-################################
+#################################
 # Author: Hui Fang
 # Purpose: ST 554 Final Project
 # Date: 4/23/2026
@@ -9,16 +9,16 @@ import os
 import time
 import pandas as pd
 
-# directory of this script
+# Get the directory where this script is located
 script_dir = os.path.dirname(os.path.abspath(__file__))
 
-# full path to stream_input inside the project folder
+# build the full path to the stream_input folder inside the project directory
 input_dir = os.path.join(script_dir, "stream_input")
 
 # 1. Ensure the input directory exists
 os.makedirs(input_dir, exist_ok=True)
 
-# 2. Clear existing files in the stream_input directory
+# 2. clear existing files in the stream_input directory
 for f in os.listdir(input_dir):
     path = os.path.join(input_dir, f)
     if os.path.isfile(path):
@@ -28,17 +28,17 @@ for f in os.listdir(input_dir):
 csv_path = os.path.join(script_dir, "power_streaming_data.csv")
 df = pd.read_csv(csv_path)
 
-print("Starting data generation...")
+print("Starting data generation...")                        # indictes the start of data generation
 
 # 4. randomly sample new files
 for i in range(20):
     sample = df.sample(n = 5)                               # sample 5 rows
-    out_path = os.path.join(input_dir, f"sample_{i}.csv")   # build the full file path for the i‑th sample inside stream_input
-    sample.to_csv(out_path, index = False)                  # write the sampled file to input folder
-    print(f"Wrote: {out_path}")                             # print the full path of the file that was just created
-    time.sleep(10)                                          # pause 10 seconds between outputs
+    out_path = os.path.join(input_dir, f"sample_{i}.csv")   # full path for the i‑th sample file
+    sample.to_csv(out_path, index = False)                  # write the sampled file to the input folder
+    print(f"Wrote: {out_path}")                             # print the full path of the file just created
+    time.sleep(20)                                          # pause 20 seconds between outputs
 
-print("Data generation completed")
+print("Data generation completed")                          # indicte the data generation completed
 
 
 
